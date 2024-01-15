b8eb3bc8ffca5fd3686aafe36fd96da703ddb9a0
Laszlo-Caballero <laszlocaballero@gmail.com>
2024-01-15 00:58:15 -0500

    Implemented modal

---
+++ YoutubeDownloader/ModalDownload/Modal.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace YoutubeDownloader.ModalDownload
+{
+    partial class Modal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            progreso = new ProgressBar();
+            btnClose = new Button();
+            txtDownload = new Label();
+            SuspendLayout();
+            // 
+            // progreso
+            // 
+            progreso.Location = new Point(34, 62);
+            progreso.Name = "progreso";
+            progreso.Size = new Size(479, 29);
+            progreso.TabIndex = 0;
+            // 
+            // btnClose
+            // 
+            btnClose.Enabled = false;
+            btnClose.Location = new Point(207, 119);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new Size(126, 38);
+            btnClose.TabIndex = 1;
+            btnClose.Text = "Aceptar";
+            btnClose.UseVisualStyleBackColor = true;
+            btnClose.Click += btnClose_Click;
+            // 
+            // txtDownload
+            // 
+            txtDownload.AutoSize = true;
+            txtDownload.Font = new Font("Segoe UI", 15F);
+            txtDownload.Location = new Point(150, 9);
+            txtDownload.Name = "txtDownload";
+            txtDownload.Size = new Size(233, 35);
+            txtDownload.TabIndex = 2;
+            txtDownload.Text = "Descarga Completa";
+            txtDownload.Visible = false;
+            // 
+            // Modal
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(539, 194);
+            Controls.Add(txtDownload);
+            Controls.Add(btnClose);
+            Controls.Add(progreso);
+            Name = "Modal";
+            Text = "Modal";
+            Load += Modal_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ProgressBar progreso;
+        private Button btnClose;
+        private Label txtDownload;
+    }
+}
---
+++ YoutubeDownloader/ModalDownload/Modal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YoutubeDownloader.ModalDownload
+{
+    public partial class Modal : Form
+    {
+        private string opcion;
+        private string url;
+        private Form elegir;
+        private Downloader download = new Downloader();
+        public Modal(string vopcion, string vurl, Form velegir)
+        {
+            InitializeComponent();
+            opcion = vopcion;
+            url = vurl;
+            elegir = velegir;
+        }
+
+        private async void Modal_Load(object sender, EventArgs e)
+        {
+            elegir.Enabled = false;
+            switch (opcion)
+            {
+                case "Audio":
+                    await download.DownloadAudio(url, progreso);
+                    btnClose.Enabled = true;
+                    txtDownload.Visible = true;
+                    break;
+                case "Video":
+                    await download.DownloadVideo(url, progreso);
+                    btnClose.Enabled = true;
+                    txtDownload.Visible= true;
+                    break;
+                case "Playlist":
+                    await download.DowloadPlaylist(url, progreso);
+                    btnClose.Enabled = true;
+                    txtDownload.Visible = true;
+                    break;
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            elegir.Enabled = true;
+            this.Close();
+        }
+    }
+}
