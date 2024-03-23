3bc7e2ff4f8f2014aebc4f239177ffdb2b87af35
Laszlo-Caballero <laszlocaballero@gmail.com>
2024-03-22 20:01:55 -0500

    Convertir webm a mp3

---
+++ YoutubeDownloader/Convertir/Convertir.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NAudio.Lame;
+using NAudio.Wave;
+
+namespace YoutubeDownloader.ffmpeg
+{
+    internal class Convertir
+    {
+        public void ConvertToMp3(string inputfile, string outputfile)
+        {
+            try
+            {
+                using(var reader = new AudioFileReader(inputfile))
+                {
+                     using (var writer = new LameMP3FileWriter(outputfile, reader.WaveFormat, LAMEPreset.VBR_50))
+                    {
+                        reader.CopyTo(writer);
+                    }
+                }
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
